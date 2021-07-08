14d72db1bb65a0e45779b7b61375f6265e942d6e
d4talouh <holdentlol@gmail.com>
2021-07-08 17:58:45 +0300

    testing debug mode

--- planeks/settings.py
+++ planeks/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-z+fu^l9801@efldo%bt+8lsbrnr#o7mjfe=&w35i!=5fhia+-e
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.get True
 DEBUG = os.environ.get('DEBUG', True)
-
+print(DEBUG)
 ALLOWED_HOSTS = ['fake-csv-d4t-alouh.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Application definition
